0305236b3d5ed1f36d7214403e67eda2ca59a5ec
Cooper <ecoope23@cci6wsjhd3aws.uncc.edu>
2018-10-08 10:25:54 -0400

    add gmaps

---
+++ script.js
@@ -0,0 +1,16 @@
+var map = new GMaps({
+    el: '#map',
+    lat:  35.4999157,-
+    lng: -80.625601
+});
+
+map.addMarker({
+    lat: 35.4999157,
+    lng: -80.625601,
+    title: 'David H. Murdock Research Institute',
+    infoWindow: {
+      content: 'David H. Murdock Research Institute'
+    }
+});
+
+map.setZoom(16);
